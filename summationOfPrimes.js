e8280b7ca64cb1b2fed2eb317c7f6b8653f14c2d
kmietotwy <kmietotwy@gmail.com>
2019-04-24 09:16:41 -1000

    completed using for loops

--- summationOfPrimes.js
+++ summationOfPrimes.js
@@ -1,3 +1,25 @@
 module.exports = function(limit) {
   // do work here
-}
+  let sum = 0;
+
+  function checkPrime(num) {
+    if (num <= 1) {
+      return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for (let i = 0; i < limit; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+};
